93652a1aafba4096564d0b282fabfb98f35b1e29
Khaled Saleh <2664083@tcs.com>
2023-07-18 14:51:58 +0200

    Implementing HATEOAS for REST API

--- src/main/java/com/khaledsaleh/restapispringboot/user/UserResource.java
+++ src/main/java/com/khaledsaleh/restapispringboot/user/UserResource.java
@@ -1,12 +1,17 @@
 package com.khaledsaleh.restapispringboot.user;
 
 import jakarta.validation.Valid;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
 import java.util.List;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 public class UserResource {
@@ -19,12 +24,15 @@ public class UserResource {
         return service.findAll();
     }
     @GetMapping("/users/{userIndex}")
-    public User retrieveUser(@PathVariable int userIndex){
+    public EntityModel<User> retrieveUser(@PathVariable int userIndex){
         User user = service.findOne(userIndex);
         if (user==null){
             throw new UserNotFoundException("id:"+userIndex);
         }
-        return user;
+        EntityModel<User> entityModel = EntityModel.of(user);
+        WebMvcLinkBuilder link = linkTo(methodOn(this.getClass()).retrieveAllUsers());
+        entityModel.add(link.withRel("all-users"));
+        return entityModel;
     }
 
     @PostMapping("/users")
